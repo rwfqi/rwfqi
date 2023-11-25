d9e9826adaebe59ed023443b5c68af86976c9c79
rwfqi <116065507+rwfqi@users.noreply.github.com>
2023-11-25 20:45:09 +0700

    Update and rename tugasqueue.java to meow.java

---
+++ meow.java
@@ -0,0 +1,7 @@
+public class meow {
+    public static void main(String[] args) {
+        System.out.println("Meow, Mew Meow!");
+        System.out.println("Meow Miaw maw mawrr.");
+        System.out.println("Meow!");
+    }
+}
